4a230b9509eaf85e878f391ef3c10932a6010587
Hari Shanmuga Raja <harishanmugaraja@rently.com>
2021-06-15 16:09:06 +0530

    before devise

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,4 +1,4 @@
 class Hotel < ApplicationRecord
-  has_many :logs
-  has_many :rooms
+#  has_many :logs
+#  has_many :rooms
 end
--- app/models/log.rb
+++ app/models/log.rb
@@ -1,4 +1,4 @@
 class Log < ApplicationRecord
-  belongs_to :hotel
-  belongs_to :user
+#  belongs_to :hotel
+#  belongs_to :user
 end
