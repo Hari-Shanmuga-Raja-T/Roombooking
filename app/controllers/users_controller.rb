fbb3167e837e606a54f684d3324e7601a8cc324a
Hari Shanmuga Raja <harishanmugaraja@rently.com>
2021-06-17 17:50:06 +0530

    after displaying hotels

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController<ApplicationController
   def managerdashboard
   end
+  def userhome
+    @hotels=Hotel.all
+  end
+  def userroombooking
+    @room = Room.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users , path: 'users'
   devise_for :hotels
   resources :rooms
+  get '/users/userhome', :to => "users#userhome"
+  get '/users/:id/userroombooking', :to => "users#userroombooking", as: :users_userroombooking
   root 'users#managerdashboard'
 end
