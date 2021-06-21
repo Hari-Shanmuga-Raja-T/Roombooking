32e1892c1af9e1a8f50305beca74fb0ed30e0ea0
Hari Shanmuga Raja <harishanmugaraja@rently.com>
2021-06-21 15:56:03 +0530

    after date validation

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,4 +1,6 @@
 class LogsController<ApplicationController
+    
+
     def create
         new_log = Log.new
         new_log.hotel_id = params[:hotel_id]
@@ -6,10 +8,30 @@ class LogsController<ApplicationController
         new_log.room_id = params[:room_id]
         new_log.startdate = params[:startdate].to_s
         new_log.enddate = params[:enddate].to_s
-        new_log.save
-        room = Room.find_by(roomid: params[:room_id])
-        room.update(status: 'Not available')
-        room.save
-        redirect_to '/users/userhome'
+        if valid_date_or_not(params[:startdate],params[:enddate])
+            respond_to do |format|
+                if new_log.save
+                    room = Room.find_by(roomid: params[:room_id])
+                    room.update(status: 'Not available')
+                    room.save
+                    format.html { redirect_to users_userhome_path , notice: "Successfully booked"}
+                end
+            end
+        else
+            respond_to do |format|
+                format.html { redirect_to users_userroombooking_path(Hotel.find_by(id: params[:hotel_id])) , notice: "Invalid start and enddate"}
+            end
+        end
     end
+
+    def valid_date_or_not(startdate , enddate)
+        if Date.parse(startdate) < Date.today
+           return false
+        end
+        if Date.parse(startdate) > Date.parse(enddate)
+           return false
+        end
+        return true
+     end
+
 end
--- app/models/log.rb
+++ app/models/log.rb
@@ -4,6 +4,7 @@ class Log < ApplicationRecord
    validates :room_id,presence: true
    validates :startdate,presence: true
    validates :enddate,presence: true
+  
    belongs_to :hotel
    belongs_to :user 
 end
