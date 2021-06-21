ed948ad59d64cc414b915ab8c928249d1e50d16f
Hari Shanmuga Raja <harishanmugaraja@rently.com>
2021-06-21 17:57:11 +0530

    after active admin

---
+++ app/admin/logs.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Log do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :hotel_id, :user_id, :room_id, :startdate, :enddate
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:hotel_id, :user_id, :room_id, :startdate, :enddate]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   post '/users/booking', :to => "logs#create"
   post '/users/roomsearch', :to => 'users#roomsearch'
   get 'users/history' , :to => "users#userhistory" , as: :users_history
